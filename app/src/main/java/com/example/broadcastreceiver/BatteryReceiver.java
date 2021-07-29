d38f51fed62a0bf413813b4fb312d1d859facc8d
sukithapalepu <sukitha.palepu@jci.com>
2021-07-29 20:04:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiver/BatteryReceiver.java
@@ -0,0 +1,23 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class BatteryReceiver extends BroadcastReceiver {
+    TextView tv;
+
+    BatteryReceiver(TextView tv){
+        this.tv=tv;
+
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int percentage = intent.getByteExtra("level", (byte) 0);
+        if (percentage != 0)
+        {
+         tv.setText(percentage + "%");
+        }
+    }
+}
